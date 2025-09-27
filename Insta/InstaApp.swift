25a5b84841ce981587b169dacba9cffe209c9fd1
dawidkolasinski <dawid.m.kolasinski@gmail.com>
2025-09-27 10:09:56 +0200

    Initial Commit

---
+++ Insta/InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaApp.swift
+//  Insta
+//
+//  Created by Dawid Kolasinski on 27/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
